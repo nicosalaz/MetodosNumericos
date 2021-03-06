911875cd46579db7bfd80e8a95180a526083269c
nicosalaz <71720590+nicosalaz@users.noreply.github.com>
2021-03-05 23:41:48 -0500

    05/03/2021

--- calculadora/ecuaciones_no_lineales/views.py
+++ calculadora/ecuaciones_no_lineales/views.py
@@ -1,4 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
 from django.shortcuts import render
+
 
 
 def mostar_form_biseccion(request):
@@ -8,64 +12,74 @@ def mostar_form_falsa_pos(request):
     return render(request,'ENL/form_falsa_pos.html')
 
 def resultado_biseccion(request):
-    funcion = determinar_func(str(request.POST['funcion']))
+    funcion = str(request.POST['funcion'])
     xi = request.POST['lim_inf']
     xf = request.POST['lim_sup']
     et = request.POST['error']
+    graficar_funcion(funcion)
     result,er = metodo_biseccion(funcion,xi,xf,et)
     return render(request,'ENL/resultado_biseccion.html',{'resultado':result,'error_relativo':er})
 
 def resultado_falsa_pos(request):
-    funcion = determinar_func(str(request.POST['funcion']))
+    funcion = str(request.POST['funcion'])
     xi = request.POST['lim_inf']
     xf = request.POST['lim_sup']
     et = request.POST['error']
+    graficar_funcion(funcion)
     result,er = metodo_falsa_posicion(funcion,xi,xf,et)
     return render(request,'ENL/resultado_biseccion.html',{'resultado':result,'error_relativo':er})
 
-def determinar_func(func):
-    aux = ''
-    for f in func:
-        if f == '^':
-            aux += '**'
-        else:
-            aux += f
-    return aux
+def determinar_func(func,valor):
+    ecuacion = sp.sympify(func)
+    simbolo = sp.symbols('x')
+    result = ecuacion.evalf(subs={simbolo:float(valor)})
+    return result
 
 def metodo_biseccion(func,xi,xf,error=0.001,ite =50):
-    f = lambda x :eval(str(func))
     ri = 0
     conta = 0
-    error_calculado = 101
-    if float(f(float(xi)))*float(f(float(xf))) <= 0:
+    error_calculado = 1
+    if float(determinar_func(func,xi))*float(determinar_func(func,xf)) <= 0:
         while conta <= ite and float(error_calculado) >= float(error):
+            print(float(determinar_func(func,xi))*float(determinar_func(func,xf)),conta)
             conta+=1
             ri = (float(xi)+float(xf))/2
-            error_calculado = abs((float(ri)-float(xi))/float(ri))*100
-            if float(f(float(xi))) * float(f(float(ri))) > 0:
+            error_calculado = abs((float(ri)-float(xi))/float(ri))
+            if float(determinar_func(func,xi)) * float(determinar_func(func,ri)) > 0:
                 xi = ri
             else:
                 xf = ri
-        return '{:.3f}'.format(ri),'{:.2f}'.format(error_calculado)
+        return '{:.5f}'.format(ri),'{:.5f}'.format(error_calculado)
 
     else:
         return False,False
 
 def metodo_falsa_posicion(func,xi,xf,error=0.001,ite =50):
-    f = lambda x: eval(str(func))
     ri = 0
     conta = 0
-    error_calculado = 101
-    if float(f(float(xi)))*float(f(float(xf))) < 0:
+    error_calculado = 1
+    if float(determinar_func(func,xi))*float(determinar_func(func,xf)) <= 0:
         while conta < ite and float(error_calculado) > float(error):
             conta+=1
-            ri = float((float(xf))-((float(f(float(xf)))*(float(xf)-float(xi)))/(float(f(float(xf)))-float(f(float(xi))))))
-            error_calculado = abs((float(ri) - float(xi)) / float(ri)) * 100
-            if float(f(float(xi))) * float(f(float(ri))) > 0:
+            ri = float((float(xf))-((float(determinar_func(func,xf))*(float(xf)-float(xi)))/(float(determinar_func(func,xf))-float(determinar_func(func,xi)))))
+            error_calculado = abs((float(ri) - float(xi)) / float(ri))
+            if float(determinar_func(func,xi)) * float(determinar_func(func,ri)) > 0:
                 xi = ri
             else:
                 xf = ri
-        return '{:.3f}'.format(ri), '{:.2f}'.format(error_calculado)
+        return '{:.5f}'.format(ri), '{:.5f}'.format(error_calculado)
 
     else:
         return False, False
+
+def graficar_funcion(func):
+    ecu = sp.sympify(func)
+    sim = sp.symbols('x')
+    sp.plot(ecu,(sim,-10,10))
+# def graficar_funcion(func,xi,xf):
+#     ecu = sp.sympify(func)
+#     x = np.linspace(float(xi),float(xf))
+#     f = ecu.evalf(subs={'x':x})
+#     plt.plot(x,f)
+#     plt.grid()
+#     plt.show()
