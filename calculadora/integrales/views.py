9d0c39e52d2d656bb6e3eaee84529348c37d6c9c
nicosalaz <71720590+nicosalaz@users.noreply.github.com>
2021-04-13 20:13:04 -0500

    13/04/2021

--- calculadora/integrales/views.py
+++ calculadora/integrales/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 import sympy as sp
 import math
 import random
+from scipy import integrate
 
 
 # Create your views here.
@@ -34,7 +35,8 @@ def resultado_integrales_rectangulos(request):
     der = intregales_rectangulos_der(func, a, b, n)
     med = intregales_rectangulos_med(func, a, b, n)
     return render(request, 'integrales/resultado_integrales_rectangulos.html'
-                  , {'izq': izq, 'der': der, 'med': med, 'func': func})
+                  , {'izq': izq, 'der': der, 'med': med, 'func': func,
+                     'a':a,'b':b})
 
 
 def resultado_integrales_trapecios(request):
@@ -43,8 +45,9 @@ def resultado_integrales_trapecios(request):
     b = request.POST['ext_der']
     n = request.POST['n']
     graficar_funcion(func, a, b)
-    calculo = integrales_trapecios(func, a, b, n)
-    return render(request, 'integrales/resultado_integrales_trapecios.html', {'calculo': calculo, 'func': func})
+    calculo,error = integrales_trapecios(func, a, b, n)
+    return render(request, 'integrales/resultado_integrales_trapecios.html', {'calculo': calculo, 'func': func,
+                                                                              'a':a,'b':b,'error':error})
 
 
 def resultado_integrales_simpson_1_3(request):
@@ -55,7 +58,7 @@ def resultado_integrales_simpson_1_3(request):
     graficar_funcion(func, a, b)
     calculo, error = integralSimpson_1_3(func, a, b, n)
     return render(request, 'integrales/resultado_integrales_simpson_1_3.html',
-                  {'calculo': calculo, 'func': func, 'error': error})
+                  {'calculo': calculo, 'func': func, 'error': error,'a':a,'b':b})
 
 
 def resultado_integrales_simpson_3_8(request):
@@ -119,17 +122,18 @@ def intregales_rectangulos_der(funcion, a, b, n):
 def intregales_rectangulos_med(funcion, a, b, n):
     deltaX = (float(b) - float(a)) / float(n)
     xn = []
-    aux = (float(a) + (float(a) + deltaX)) / 2
+    aux = float(a)
     result = 0
-    for i in range(int(n)):
+    for i in range(int(n)+1):
         if aux > float(b):
             break
         xn.append(float(aux))
-        aux = (aux + (aux + deltaX)) / 2
-    # print('delta: ',deltaX)
-    # print('xn: ',xn)
-    for x in xn:
-        result += determinar_func(funcion, x)
+        aux = aux + deltaX
+    #print('delta: ',deltaX)
+    #print('xn: ',xn)
+    for x in range(0,len(xn)-1):
+        media = (float(xn[x]) + float(xn[x+1]))/2
+        result += determinar_func(funcion, media)
     # print('resultado: ', '{:.5f}'.format(result))
     result *= deltaX
     # print('resultado: ','{:.5f}'.format(result))
@@ -141,32 +145,33 @@ def integrales_trapecios(funcion, a, b, n):
     xn = []
     aux = float(a)
     result = 0
-    resultado_final = 0
-    conta = 0
     imagen_a = determinar_func(funcion, a)
     imagen_b = determinar_func(funcion, b)
+    randomico = random.uniform(0, 1)
+    epsilon = float(a) + randomico * (float(b) - float(b))
+    derivada = calcular_derivada(funcion,float(epsilon),2)
+    error = -((deltaX**3)/12)*derivada
 
     for i in range(int(n)):
-        if aux >= float(b):
-            break
         xn.append(float(aux))
         aux += deltaX
-    # print('xn: ',xn)
-    for x in xn:
-        result += determinar_func(funcion, float(x))
-        # print(determinar_func(funcion, x))
-    # print('resultado: ', '{:.5f}'.format(result))
-    resultado_final = (float(imagen_a) + (2 * result) + float(imagen_b)) * (deltaX / 2)
-    # print('resultado: ','{:.5f}'.format(resultado_final))
+    #print('xn: ',xn)
+    for x in range(1,len(xn)):
+        result += determinar_func(funcion,xn[x])*2
+    result+= (float(determinar_func(funcion,a))+float(determinar_func(funcion,b)))
+    result *= (deltaX/2)
+    #     # print(determinar_func(funcion, x))
+    # # print('resultado: ', '{:.5f}'.format(result))
+    resultado_final =result
+    # # print('resultado: ','{:.5f}'.format(resultado_final))
 
-    return '{:.5f}'.format(resultado_final)
+    return '{:.5f}'.format(resultado_final),error
 
-
-def calcular_derivada(func, valor):
+def calcular_derivada(func, valor,num_int):
     funcion = sp.sympify(func)
     x = sp.symbols('x')
     numero = valor
-    r_pd = float(sp.diff(funcion, x, 4).evalf(subs={x: numero}))
+    r_pd = float(sp.diff(funcion, x, num_int).evalf(subs={x: numero}))
 
     return r_pd
 
@@ -179,7 +184,8 @@ def integralSimpson_1_3(funcion, a, b, n):
     valorB = float(b)
     valorN = aux
     sumatoria = 0
-    epsilon = random.uniform(0, 1)
+    randomico = random.uniform(0, 1)
+    epsilon = float(a)+randomico*(float(b)-float(b))
     delta = ((valorB - valorA) / valorN)
 
     for i in range(valorN + 1):
@@ -196,7 +202,7 @@ def integralSimpson_1_3(funcion, a, b, n):
 
     resultado = (delta / 3) * sumatoria
 
-    derivada4 = calcular_derivada(funcion, float(epsilon))
+    derivada4 = calcular_derivada(funcion, float(epsilon),4)
 
     error = -(((delta ** 5) / 90) * derivada4)
 
@@ -212,7 +218,8 @@ def integralSimpson_3_8(funcion, a, b, n):
 
     valorA = float(a)
     valorB = float(b)
-    epsilon = random.uniform(0, 1)
+    randomico = random.uniform(0, 1)
+    epsilon =  float(a)+randomico*(float(b)-float(b))
     delta = ((valorB - valorA) / 3)
 
     x0 = valorA
@@ -225,7 +232,7 @@ def integralSimpson_3_8(funcion, a, b, n):
     fx2 = determinar_func(funcion, x2)
     fx3 = determinar_func(funcion, x3)
 
-    derivada4 = calcular_derivada(funcion, float(epsilon))
+    derivada4 = calcular_derivada(funcion, float(epsilon),4)
 
     error = -((3 / 80) * (delta ** 5)) * derivada4
 
